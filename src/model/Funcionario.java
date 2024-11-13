0cf01024c5da2639ccd979048bcd016eb8110acc
Brenda Leal <100167546+endyyxs@users.noreply.github.com>
2024-11-13 03:03:04 -0300

    Update Funcionario.java

--- src/model/Funcionario.java
+++ src/model/Funcionario.java
@@ -2,6 +2,8 @@ package model;
 
 import java.time.LocalDate;
 
+import javax.swing.JOptionPane;
+
 public class Funcionario extends Usuario {
 	private String codigoFuncionario;
 	private String cargo;
@@ -15,15 +17,33 @@ public class Funcionario extends Usuario {
 		this.senha = senha;
 	}
 	
+	
+	public String getCodigoFuncionario() {
+		return codigoFuncionario;
+	}
+
+	public String getCargo() {
+		return cargo;
+	}
+
+	public void setCargo(String cargo) {
+		this.cargo = cargo;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
 	public void abrirConta(Conta conta) {
-		
+		JOptionPane.showMessageDialog(null, "Conta aberta para o cliente: " + conta.getCliente().getNome());
 	}
 	
 	public void encerrarConta(Conta conta) {
-		
+		JOptionPane.showMessageDialog(null, "Conta " + conta.getNumero() + " encerrada.");
 	}
 	
 	public Conta consultarDadosConta(int numeroConta) {
+		JOptionPane.showMessageDialog(null, "Consultando dados da Conta " + numeroConta);
 		return null;
 		
 	}
