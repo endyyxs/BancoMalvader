271191ef10a514b9a8bd268d8a2c925c2eff7fe8
Brenda Leal <100167546+endyyxs@users.noreply.github.com>
2024-11-20 23:51:41 -0300

    Add files via upload

---
+++ src/util/DBUtil.java
@@ -0,0 +1,39 @@
+package util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBUtil {
+	private static final String URL = "jdbc:mysql://localhost/bancomalvada";
+	private static final String USER = "root";
+	private static final String PASSWORD = "c@tolic@";
+	private static Connection conexao;
+	
+	private DBUtil() {
+		
+	}
+	
+	public static Connection getConnection() {
+		 if (conexao == null) {
+		 		try {
+	                conexao = DriverManager.getConnection(URL, USER, PASSWORD);
+	            } catch (SQLException e) {
+	                e.printStackTrace();
+	                throw new RuntimeException("Erro ao conectar ao banco de dados", e);
+	              }
+	     }
+	     return conexao;
+	}
+	
+	public static void closeConnection() {
+        if (conexao != null) {
+            try {
+                conexao.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+              }
+        }
+	}
+
+}
