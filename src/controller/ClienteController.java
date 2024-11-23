06f03965dc0356d5d95e2cc20615d84529365699
EduardoL15 <duduzinho_luciano-15@hotmail.com>
2024-11-23 19:53:40 -0300

    cliente

--- src/controller/ClienteController.java
+++ src/controller/ClienteController.java
@@ -1,6 +1,9 @@
 package controller;
 
+import java.util.List;
+
 import DAO.ClienteDAO;
+import model.Cliente;
 
 public class ClienteController {
 	private ClienteDAO DAO;
@@ -9,16 +12,51 @@ public class ClienteController {
 		this.DAO = new ClienteDAO();
 	}
 	
-	public double consultarSaldo(int ClienteId) {
-		return DAO.consultarSaldo(ClienteId);
+	public double consultarSaldo(Cliente cliente) {
+		try {
+			return DAO.consultarSaldo(cliente);
+		} catch(Exception e) {
+			System.out.println("Erro ao consultar saldo: " + e.getMessage());
+			return 0;
+		}
+		
 	}
 	
-	public void depositar(int clienteId, double valor) {
+	public void depositar(Cliente cliente, double valor) {
 		if (valor <= 0) {
 			throw new IllegalArgumentException("O valor para depósito deve ser positivo.");
 		}
-		if (DAO.depositar(clienteId, valor)) {
+		if (DAO.depositar(cliente, valor)) {
 			System.out.println("Depósito realizado com sucesso.");
 		}
 	}
+	public boolean sacar(Cliente cliente, double valor) {
+        if (valor <= 0) {
+            System.err.println("O valor para saque deve ser maior que zero.");
+            return false;
+        }
+        try {
+            return DAO.sacar(cliente, valor);
+        } catch (Exception e) {
+            System.err.println("Erro ao realizar saque: " + e.getMessage());
+            return false;
+        }
+    }
+	public List<String> consultarExtrato(Cliente cliente) {
+        try {
+            return DAO.consultarExtrato(cliente);
+        } catch (Exception e) {
+            System.err.println("Erro ao consultar extrato: " + e.getMessage());
+            return null;
+        }
+    }
+	public double consultarLimite(Cliente cliente) {
+        try {
+            return DAO.consultarLimite(cliente);
+        } catch (Exception e) {
+            System.err.println("Erro ao consultar limite: " + e.getMessage());
+            return 0;
+        }
+    }
+
 }
